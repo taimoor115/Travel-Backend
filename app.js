7daea08f1b0d72273a0d66491cdffd33bd3ca2da
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-28 02:56:56 +0500

    Error Page

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { status = 500, message = "Something went wrong..." } = err;
-  res.status(status).send(message);
+  res.status(status).render("error", { message });
 });
 
 // Server Connection
