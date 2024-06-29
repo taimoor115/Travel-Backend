86f1fdbeb0e4a22461b4dcae80f6e88e51d9eab3
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-30 02:32:49 +0500

    Cookie Options[add expire date to cookie]

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ const sessionOptions = {
   secret: "mysecretkey",
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
 // middlewares
