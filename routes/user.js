41df083d7a00e2b994371697bbb09db5cbcfa802
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-30 18:46:31 +0500

    Save User detail into DB

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup");
 });
 
+router.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = new User({
+      username,
+      email,
+    });
+    const result = await User.register(user, password);
+    console.log(result);
+    req.flash("success", "Welcome to Traveller!");
+    res.redirect("/listings");
+  } catch (error) {
+    req.flash("error", error.message);
+    res.redirect("/signup");
+  }
+});
 module.exports = router;
