61c7db45b799a04560b75da683dabd6d1a2f8b29
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-30 19:10:36 +0500

    Login User

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup");
@@ -15,11 +16,27 @@ router.post("/signup", async (req, res) => {
     });
     const result = await User.register(user, password);
     console.log(result);
-    req.flash("success", "Welcome to Traveller!");
-    res.redirect("/listings");
+    req.flash("success", "Sign up successfully!");
+    res.redirect("/login");
   } catch (error) {
     req.flash("error", error.message);
     res.redirect("/signup");
   }
 });
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome to Traveller ...");
+    res.redirect("/listings");
+  }
+);
 module.exports = router;
