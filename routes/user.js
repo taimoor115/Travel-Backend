78627e4ae8172d00480880e76656eb8e2a1fb7b6
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-07-01 02:05:40 +0500

    Logout user

--- routes/user.js
+++ routes/user.js
@@ -40,4 +40,14 @@ router.post(
     res.redirect("/listings");
   }
 );
+
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You are logged out successfully...");
+    res.redirect("/listings");
+  });
+});
 module.exports = router;
